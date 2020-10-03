e19ee6400325a65e23acc52d97be5ec988b9e52e
Alex <budnikoleksii@gmail.com>
2020-10-03 20:13:02 +0300

    sreialize cities and countries

--- src/js/store/locations.js
+++ src/js/store/locations.js
@@ -5,6 +5,7 @@ class Locations {
         this.api = api;
         this.countries = null;
         this.cities = null;
+        this.shortCitiesList = {};
     }
 
     async init() {
@@ -14,10 +15,44 @@ class Locations {
         ]);
 
         const [countries, cities] = response;
-        this.countries = countries;
-        this.cities = cities;
+        this.countries = this.serializeCountries(countries);
+        this.cities = this.serializeCities(cities);
+        this.shortCitiesList = this.createShortCititesList(this.cities);
 
         return response;
+    }
+
+    createShortCititesList(cities) {
+        // { 'City, Country': null }
+        // Object.entries => [key, value]
+        return Object.entries(cities).reduce((acc, [key]) => {
+            acc[key] = null;
+            return acc;
+        }, {});
+    }
+
+    serializeCountries(countries) {
+        // { 'Country code': { ... } }
+        return countries.reduce((acc, country) => {
+            acc[country.code] = country;
+            return acc;
+        }, {});
+    }
+
+    serializeCities(cities) {
+        // { 'City name, Country name': { ... } }
+        return cities.reduce((acc, city) => {
+            const country_name = this.getCountryNameByCode(city.country_code);
+            const city_name = city.name || city.name_translations.en;
+            const full_name = `${city_name},${country_name}`;
+            acc[full_name] = city;
+            return acc;
+        }, {});
+    }
+
+    getCountryNameByCode(code) {
+        // { 'Country code': { ... } }
+        return this.countries[code].name;
     }
 
     getCitiesByCountryCode(code) {
@@ -28,3 +63,7 @@ class Locations {
 const locations = new Locations(api);
 
 export default locations;
+
+// { 'City, Country': null }
+// [{}, {}]
+// { 'City': {...} } => cities[code]
